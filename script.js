e78036c67767eae63624e791908fc2d49526b4b7
Noor-26 <nk26982698@gmail.com>
2022-03-01 11:29:29 +0600

    condition of applying only 20 item is done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const getPhoneName = () => {
     const searchInput = document.getElementById('search-input')
-    const searchValue = searchInput.value;   // got input value
+    const searchValue = searchInput.value;   
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
     fetch(url)
@@ -11,24 +11,29 @@ const getPhoneName = () => {
 // function of displaying the phones
 
 const displayPhone = phones => {
-    // console.log(phones);
-    const mainDiv = document.getElementById('display-phone')
-    phones.forEach(phone => {
-        const div = document.createElement('div')
-        div.innerHTML = `
-        <div class="col h-100">
+  const mainDiv = document.getElementById('display-phone')
+    let i = 0
+      phones.forEach(phone => {
+        i++
+        if (i <= 20) {
+          const div = document.createElement('div')
+          div.innerHTML = `
+          <div class="col h-100">
           <div class="card h-100">
-            <img src="${phone.image}" class="card-img-top p-5" alt="...">
-            <div class="card-body text-center">
-              <h5 class="card-title">Name : ${phone.phone_name}</h5>
-              <h6>Brand : ${phone.brand}</h6>
-              <button class="btn btn-success" onclick="loadOneItem('${phone.slug}')">Detail</button>
+          <img src="${phone.image}" class="card-img-top p-5" alt="...">
+          <div class="card-body text-center">
+          <h5 class="card-title">Name : ${phone.phone_name}</h5>
+                <h6>Brand : ${phone.brand}</h6>
+                <button class="btn btn-success" onclick="loadOneItem('${phone.slug}')">Detail</button>
+              </div>
             </div>
           </div>
-        </div>
-        `
-        mainDiv.appendChild(div)
-    });
+          `
+          mainDiv.appendChild(div)
+        }
+        });
+        
+
 }
 
 // displaying selected item
@@ -94,7 +99,7 @@ const showSensors = sensors =>{
     li.innerHTML = sensor  
     ul.appendChild(li)
   });
-  // console.log(ul);
+
   sensorId.innerHTML = ul.innerHTML
 }
 
