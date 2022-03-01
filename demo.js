8e5313cc3ec5f73cd1da6c6c77f955224fc44aba
Noor-26 <nk26982698@gmail.com>
2022-03-01 11:02:33 +0600

    sensors features others added

---
+++ demo.js
@@ -0,0 +1,9 @@
+const arr = ['Face ID', 'accelerometer', 'gyro', 'proximity', 'compass', 'barometer']
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    // console.log(element);
+    
+}
+arr.forEach(element => {
+    console.log(element);
+});
--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const displayPhone = phones => {
             <div class="card-body text-center">
               <h5 class="card-title">Name : ${phone.phone_name}</h5>
               <h6>Brand : ${phone.brand}</h6>
-              <button class="btn btn-success" onclick="displayOneItem('${phone.slug}')">Detail</button>
+              <button class="btn btn-success" onclick="loadOneItem('${phone.slug}')">Detail</button>
             </div>
           </div>
         </div>
@@ -33,9 +33,69 @@ const displayPhone = phones => {
 
 // displaying selected item
 
-const displayOneItem = id => {
+const loadOneItem = id => {
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayOneItem(data.data))
 }
+
+const selectedDiv = document.getElementById('selected-item')
+selectedDiv.style.display = 'none'
+
+const displayOneItem = phoneId => {
+  console.log(phoneId);
+  selectedDiv.style.display = "grid"
+  const div = document.createElement('div')
+  div.innerHTML = `
+  <div class="row g-0">
+          <div class="col-md-4 text-center ">
+            <img src="${phoneId.image}" class="img-fluid rounded-start p-4 " alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body ">
+            
+              <h5 class="card-title text-center">Name : ${phoneId.name}</h5>
+              <p class="text-center">release date : ${phoneId.releaseDate}</p>
+
+              <div >
+              <h5>Main features</h5>
+                <p>Memory : ${phoneId.mainFeatures.memory}</p>
+                <p>Chipset : ${phoneId.mainFeatures.chipSet} </p>
+                <p>Display size : ${phoneId.mainFeatures.displaySize} </p>
+                <p>Storage : ${phoneId.storage}</p> 
+              </div>
+              <h5>Sensors</h5>
+              <div id="sensors">
+                  
+              </div>
+              
+              <h5 class="mt-3">Other features</h5>
+              <p>Bluetooth : ${phoneId.others.Bluetooth}</p>
+              <p>GPS : ${phoneId.others.GPS}</p>
+              <p>NFC : ${phoneId.others.NFC}</p>
+              <p>Radio : ${phoneId.others.Radio}</p>
+              <p>USB : ${phoneId.others.USB}</p>
+              <p>WLAN : ${phoneId.others.WLAN}</p>
+
+              
+            </div>
+          </div>
+        </div>
+  `
+  selectedDiv.appendChild(div)
+  showSensors(phoneId.mainFeatures.sensors)
+}
+
+const showSensors = sensors =>{
+  const ul = document.createElement('ul')
+  const sensorId = document.getElementById('sensors')
+  sensors.forEach(sensor => {
+    const li = document.createElement('li')
+    li.innerHTML = sensor  
+    ul.appendChild(li)
+  });
+  // console.log(ul);
+  sensorId.innerHTML = ul.innerHTML
+}
+
 
