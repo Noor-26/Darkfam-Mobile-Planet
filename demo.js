0480c9a499462d5c3ae9ad8c74c953eed804681a
Noor-26 <nk26982698@gmail.com>
2022-03-01 15:26:55 +0600

    assingment completed

--- demo.js
+++
@@ -1,9 +0,0 @@
-const arr = ['Face ID', 'accelerometer', 'gyro', 'proximity', 'compass', 'barometer']
-for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    // console.log(element);
-    
-}
-arr.forEach(element => {
-    console.log(element);
-});
--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const emptyText = document.getElementById('empty-input');
+const emptyItem = document.getElementById('empty-item');
+
 const getPhoneName = () => {
-    const searchInput = document.getElementById('search-input')
-    const searchValue = searchInput.value;
-    searchInput.value = ''   
+  const searchInput = document.getElementById('search-input')
+  const searchValue = searchInput.value;
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayPhone(data.data))
+   fetch(url)
+   .then(res => res.json())
+   .then(data => displayPhone(data.data))
+  
+   if (searchValue.length == 0) {
+     emptyText.style.display ="block"
+   }
+   else{
+    emptyText.style.display ="none"
+   }
 };
 
 // function of displaying the phones
@@ -14,52 +23,61 @@ const getPhoneName = () => {
 const displayPhone = phones => {
   const mainDiv = document.getElementById('display-phone')
   mainDiv.textContent = ''
-    let i = 0
-      phones.forEach(phone => {
-        i++
-        if (i <= 20) {
-          const div = document.createElement('div')
-          div.innerHTML = `
-          <div class="col h-100">
-          <div class="card h-100">
-          <img src="${phone.image}" class="card-img-top p-5" alt="...">
-          <div class="card-body text-center">
-          <h5 class="card-title">Name : ${phone.phone_name}</h5>
-                <h6>Brand : ${phone.brand}</h6>
-                <button class="btn btn-success" onclick="loadOneItem('${phone.slug}')">Detail</button>
+
+  const searchInput = document.getElementById('search-input')
+  const searchValue = searchInput.value;
+  let i = 0;
+
+  if(phones.length==0 && searchValue.length >0 ){
+ emptyItem.style.display = "block"
+}
+else{
+ emptyItem.style.display = "none"
+}
+
+  phones.forEach(phone => {
+      i++;
+       if (i <= 20) {
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <div class="col h-100">
+            <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top p-5" alt="...">
+            <div class="card-body text-center">
+            <h5 class="card-title">Name : ${phone.phone_name}</h5>
+                  <h6>Brand : ${phone.brand}</h6>
+                  <button class="btn btn-success" onclick="loadOneItem('${phone.slug}')">Detail</button>
+                </div>
               </div>
             </div>
-          </div>
-          `
-          mainDiv.appendChild(div)
-        }
-        });
-        
+            `
+        mainDiv.appendChild(div)
+      }
+    });
 
-}
+    //input of search Value deleted
+   searchInput.value = ''
+  }
 
 // displaying selected item
 
 const loadOneItem = id => {
-    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
     .then(res => res.json())
     .then(data => displayOneItem(data.data))
 }
 
-
-const selectedDiv = document.getElementById('selected-item')
-selectedDiv.style.display = 'none'
-
 const displayOneItem = phoneId => {
+  const selectedDiv = document.getElementById('selected-item')
+  selectedDiv.style.display = "grid"
   selectedDiv.textContent = ""
-  console.log(phoneId);
   let release = phoneId.releaseDate
 
-     if (release == '') {
-  release = "Release date not found"
-    }
+  //release date error solved
+  if (release == '') {
+    release = "Release date not found"
+  }
 
-  selectedDiv.style.display = "grid"
   const div = document.createElement('div')
   div.innerHTML = `
   <div class="row g-0">
@@ -70,8 +88,8 @@ const displayOneItem = phoneId => {
             <div class="card-body ">
             
               <h5 class="card-title text-center">${phoneId.name}</h5>
-              <p class="text-center" id="releaseDate">${release}</p>
-
+              <p class="text-center " id="releaseDate">${release}</p>
+              <p class="fw-bold"> Brand : ${phoneId.brand}</p>
               <div >
               <h5>Main features</h5>
                 <p>Memory : ${phoneId.mainFeatures.memory}</p>
@@ -80,43 +98,44 @@ const displayOneItem = phoneId => {
                 <p>Storage : ${phoneId.mainFeatures.storage}</p> 
               </div>
               <h5>Sensors</h5>
+
               <div id="sensors">
                   
               </div>
-              
+
               <h5 class="mt-3">Other features</h5>
+
               <div id="others">
               
               </div>
-              
             </div>
           </div>
         </div>
   `
-  
+
   selectedDiv.appendChild(div)
-  showSensors(phoneId.mainFeatures.sensors )
+  showSensors(phoneId.mainFeatures.sensors)
   othersname(phoneId.others)
 }
 
-const showSensors = sensors =>{
+// function for showing sensors
+const showSensors = sensors => {
   const ul = document.createElement('ul')
   const sensorId = document.getElementById('sensors')
-  
+
   sensors.forEach(sensor => {
     const li = document.createElement('li')
-    li.innerHTML = sensor  
+    li.innerHTML = sensor
     ul.appendChild(li)
   });
-  
   sensorId.innerHTML = ul.innerHTML
 }
 
-
+// function for showing other features
 const othersname = idName => {
   const otherId = document.getElementById('others')
-
   const div = document.createElement('div')
+
   for (const other in idName) {
     const p = document.createElement('p')
     p.innerHTML = `
@@ -124,12 +143,12 @@ const othersname = idName => {
     `
     div.appendChild(p)
   }
- otherId.innerHTML = div.innerHTML
+  otherId.innerHTML = div.innerHTML
 
- // massage of no other features 
- if (otherId.innerHTML == '') {
-  otherId.innerHTML = "Other features not avalaible"
- }
+  // massage of no other features 
+  if (otherId.innerHTML == '') {
+    otherId.innerHTML = "Other features not avalaible"
+  }
 }
 
 
